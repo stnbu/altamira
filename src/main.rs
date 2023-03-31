40806f6f6b99aae61ded28b2e9ba42b6b92ac413
Mike Burr <mb@unintuitive.org>
2023-03-31 14:17:41 -0600

    Y

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 use bevy::{
     input::mouse::{MouseMotion, MouseWheel},
-    pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
     prelude::*,
     render::{render_resource::WgpuFeatures, settings::WgpuSettings, RenderPlugin},
 };
 
-mod torus;
-use torus::*;
+mod texture;
+//use texture::*;
 
 const EPSILON: f32 = 0.00001;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(RenderPlugin {
-            wgpu_settings: WgpuSettings {
-                features: WgpuFeatures::POLYGON_MODE_LINE,
-                ..default()
-            },
-        }))
-        .add_plugin(WireframePlugin)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_startup_system(setup)
         .add_startup_system(spawn_camera)
         .add_system(control)
@@ -82,43 +75,34 @@ fn control(
 
 fn setup(
     mut commands: Commands,
-    mut wireframe_config: ResMut<WireframeConfig>,
+    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    wireframe_config.global = false;
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             shadows_enabled: false,
-            ..default()
+            ..Default::default()
         },
+        ..Default::default()
+    });
+
+    let material = materials.add(StandardMaterial {
+        base_color_texture: Some(images.add(texture::texture())),
         ..default()
     });
 
-    let torus = Torus {
-        radius: 0.5,
-        ring_radius: 0.25,
-        ..Default::default()
-    };
-    use bevy::render::mesh::VertexAttributeValues;
-    //
-    let mut colorful_cube = Mesh::from(torus);
-    if let Some(VertexAttributeValues::Float32x3(positions)) =
-        colorful_cube.attribute(Mesh::ATTRIBUTE_POSITION)
-    {
-        let colors: Vec<[f32; 4]> = positions
-            .iter()
-            .map(|[r, g, b]| [(1. - *r) / 2., (1. - *g) / 2., (1. - *b) / 2., 1.])
-            .collect();
-        colorful_cube.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
-    }
     commands.spawn(PbrBundle {
-        mesh: meshes.add(colorful_cube),
-        // This is the default color, but note that vertex colors are
-        // multiplied by the base color, so you'll likely want this to be
-        // white if using vertex colors.
-        material: materials.add(Color::rgb(1., 1., 1.).into()),
+        mesh: meshes.add(
+            shape::Torus {
+                radius: 0.5,
+                ring_radius: 0.25,
+                ..Default::default()
+            }
+            .into(),
+        ),
+        material,
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
+        ..Default::default()
     });
 }
